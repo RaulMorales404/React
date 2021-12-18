bcdd0c2d6beaa9a55d79fa187bc20a7122aa1f7f
RaulMorales404 <r20160106@gmail.com>
2021-12-17 18:16:01 -0600

    clean the proyect for incluy the react router

--- redux/src/components/SearchBox/CustomButtons.js
+++
@@ -1,9 +0,0 @@
-import './Style.css';
-export const CustomButtons = ({ className, action, text }) => {
-  return (
-    <button className={className} onClick={action}>
-      {text}
-    </button>
-  );
-};
-export default CustomButtons;
